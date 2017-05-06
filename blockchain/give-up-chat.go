3499469f8aba16bf973fcfb0223bacd31036fad1
Chi <contractswif@hotmail.com>
2017-05-06 15:10:20 +0800

    no message

--- blockchain/give-up-chat.go
+++ blockchain/give-up-chat.go
@@ -60,10 +60,10 @@ func main() {
     // }
 	// fmt.Println("here", string(obj))
 
-	// err := shim.Start(new(SimpleChaincode))
-	// if err != nil {
-	// 	fmt.Printf("Error starting Simple chaincode: %s", err)
-	// }
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
 }
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
